5c2e23a4ab60a549b146fbcd8a0a0d0aedcf431b
kuaner <kuaner@gmail.com>
2026-03-26 14:17:27 +0800

    fix: update cc-reader dependency version to 1.0.0

--- Package.swift
+++ Package.swift
@@ -14,7 +14,7 @@ let package = Package(
         )
     ],
     dependencies: [
-        .package(url: "https://github.com/kuaner/cc-reader.git", from: "0.2.3"),
+        .package(url: "https://github.com/kuaner/cc-reader.git", from: "1.0.0"),
     ],
     targets: [
         .executableTarget(
